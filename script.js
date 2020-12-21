71cae14dbfe26fa9e27b27c7d4abe38fb1674d35
Tim Keller <timothy.m.keller@gmail.com>
2020-12-20 22:08:02 -0800

    Added new containers #main_card, #forecast_card, #forecast_header, and #card_row. Removed list item logic from #submit_search click event and instead put them into the AJAX call. Removed several console.logs used for testing. Switch the JS to update the #main_card instead of creating a new element. Added for loop to create forecast cards. Added a conditional as an argument to determine whether the AJAX call should add list elements below the city search input. Added an input error for when the user puts in an invalid city. Finally, added styling for the new elements.

--- script.js
+++ script.js
@@ -3,19 +3,13 @@ var APIkey = "d496986a7fe0f4f18c2555bc37b9566b";
 $( "#submit_search" ).click( function( event ) {
     event.preventDefault();
     if ( $( "#city_search" ).val() ) {
-        var newCity = $( "<li>" );
-        newCity.addClass( "sidebar_city" );
-        newCity.text( $( "#city_search" ).val() );
+        queryCurrentWeather( $( "#city_search" ).val(), APIkey, "imperial", true );
         $( "#city_search" ).val( "" );
-        $( "#card_list" ).append( newCity );
-
-        $( ".sidebar_city" ).click( function() {
-            console.log( $( this ).text() );
-        });
-    }
+    };
 });
 
-function queryCurrentWeather( cityName, apiKey, units ) {
+// Queries the OpenWeatherMap API for the current weather at the specified location, and updates the center "main" card with the data.
+function queryCurrentWeather( cityName, apiKey, units, addListElement = false ) {
 
     var cwURL = `https://api.openweathermap.org/data/2.5/weather?q=${ cityName }&appid=${ apiKey }`;
 
@@ -26,7 +20,6 @@ function queryCurrentWeather( cityName, apiKey, units ) {
         url: cwURL,
         method: "GET"
     }).then( function( cwResponse ) {
-        console.log( cwResponse );
         lat = cwResponse.coord.lat;
         lon = cwResponse.coord.lon;
 
@@ -41,20 +34,17 @@ function queryCurrentWeather( cityName, apiKey, units ) {
             var uvIndex = ocResponse.current.uvi;
             var temp = Math.round( ocResponse.current.temp * 10 ) / 10;
     
-            var centerCard = $( "<section>" ).attr( "id", "main_card" );
-            var cityP = $( `<h1>${ cwResponse.name } ${ d.getMonth() + 1 }/${ d.getDate() }/${ d.getFullYear() }</h1>`);
+            // Update center card.
+            $( "#content_header" ).text( `${ cwResponse.name } ${ d.getMonth() + 1 }/${ d.getDate() }/${ d.getFullYear() }`);
             var tempP = $( `<p>Temperature: ${ temp } °F<p>` );
             var humidityP = $( `<p>Humidity: ${ ocResponse.current.humidity }%</p>` );
             var windP = $( `<p>Wind speed: ${ ocResponse.current.wind_speed } MPH</p>` );
             var uvIndexP = $( `<p>UV Index: <span id="uv_index">${ uvIndex }</span></p>`);
     
-            $( centerCard ).append( cityP );
-            $( centerCard ).append( tempP );
-            $( centerCard ).append( humidityP );
-            $( centerCard ).append( windP );
-            $( centerCard ).append( uvIndexP );
-            $( "#content_main" ).text( "" );
-            $( "#content_main" ).append( centerCard );
+            $( "#main_card" ).append( tempP );
+            $( "#main_card" ).append( humidityP );
+            $( "#main_card" ).append( windP );
+            $( "#main_card" ).append( uvIndexP );
             if ( uvIndex < 3 ) {
                 $( "#uv_index" ).css( "background-color", "green" );
             } else if ( uvIndex < 6 ) {
@@ -64,8 +54,50 @@ function queryCurrentWeather( cityName, apiKey, units ) {
             } else {
                 $( "#uv_index" ).css( "background-color", "red" );
             }
+
+            $( "#forecast_header" ).text( "5-Day Forecast:" );
+            for ( var i = 0; i < 5; i++ ) {
+                d = new Date( d );
+                d.setDate( d.getDate() + 1 );
+                console.log( ocResponse.daily[ i ], console.log( `${ d.getMonth() + 1 }/${ d.getDate() }/${ d.getFullYear() }` ) );
+                var dailyCard = $( "<section>" );
+                dailyCard.addClass( "daily_card col-md-2");
+                var cardDateHeader = $( "<h5>" ).text( `${ d.getMonth() + 1 }/${ d.getDate() }/${ d.getFullYear() }` );
+                var cardIcon = $( "<span>" ).text( "Icon Placeholder" );
+                var cardTemp = $( `<p>Temperature: ${ Math.round( ocResponse.daily[ i ].temp.day ) } °F<p>` );
+                var cardHumidity = $( `<p>Humidity: ${ ocResponse.daily[ i ].humidity }%</p>` );
+
+                dailyCard.append( cardDateHeader );
+                dailyCard.append( cardIcon );
+                dailyCard.append( cardTemp );
+                dailyCard.append( cardHumidity );
+                $( "#card_row" ).append( dailyCard );
+            }
+
+            if ( addListElement ) {
+                var newCity = $( "<li>" );
+                newCity.addClass( "sidebar_city" );
+                newCity.text( cwResponse.name );
+                $( "#city_search" ).val( "" );
+                $( "#card_list" ).append( newCity );
+                $( ".sidebar_city" ).click( function() {
+                    queryCurrentWeather( $( this ).text(), APIkey, "imperial", null );
+                });
+            }
+
+        }).catch( function() {
+            inputAjaxError();
         });
+    }).catch( function() {
+        inputAjaxError();
     });
+}
+
+function inputAjaxError() {
+    $( "#input_form_main" ).append( "<p id='error_msg' style='color: red;'>Could not find location</p>" );
+    setTimeout( function() {
+        $( "#error_msg" ).remove();
+    }, 3000 );
 }
 
 function queryForecast( cityName, apiKey, units ) {
@@ -76,8 +108,6 @@ function queryForecast( cityName, apiKey, units ) {
         url: queryURL,
         method: "GET"
     }).then( function( response ) {
-        console.log( response );
+        console.log( "That's an error." );
     })
 }
-
-queryCurrentWeather( "Seattle", APIkey, "imperial" );
