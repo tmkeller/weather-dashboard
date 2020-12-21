58a56a077d2906f073c61f8b6a611860b0c19159
Tim Keller <timothy.m.keller@gmail.com>
2020-12-20 22:36:54 -0800

    Fixed bug where forecast cards and main card were being duplicated instead of replaced and added with each new city query. Also fixed bug with the queryCurrentWeather call in the .sidebar_city click event that was resulting in the header not appearing. Added CSS styling for the p tags in the daily cards.

--- script.js
+++ script.js
@@ -35,12 +35,17 @@ function queryCurrentWeather( cityName, apiKey, units, addListElement = false ) 
             var temp = Math.round( ocResponse.current.temp * 10 ) / 10;
     
             // Update center card.
-            $( "#content_header" ).text( `${ cwResponse.name } ${ d.getMonth() + 1 }/${ d.getDate() }/${ d.getFullYear() }`);
-            var tempP = $( `<p>Temperature: ${ temp } °F<p>` );
+            $( "#main_card" ).text( "" );
+            var headerH1 = $( "h1" ).text( `${ cwResponse.name } ${ d.getMonth() + 1 }/${ d.getDate() }/${ d.getFullYear() }` ).attr( "id", "content_header");
+
+            var headerH1 = $( `<h1>${ cwResponse.name } ${ d.getMonth() + 1 }/${ d.getDate() }/${ d.getFullYear() }</h1>`);
+            headerH1.attr( "id", "content_header" );
+            var tempP = $( `<p>Temperature: ${ temp } °F</p>` );
             var humidityP = $( `<p>Humidity: ${ ocResponse.current.humidity }%</p>` );
             var windP = $( `<p>Wind speed: ${ ocResponse.current.wind_speed } MPH</p>` );
             var uvIndexP = $( `<p>UV Index: <span id="uv_index">${ uvIndex }</span></p>`);
-    
+
+            $( "#main_card" ).append( headerH1 );
             $( "#main_card" ).append( tempP );
             $( "#main_card" ).append( humidityP );
             $( "#main_card" ).append( windP );
@@ -56,6 +61,7 @@ function queryCurrentWeather( cityName, apiKey, units, addListElement = false ) 
             }
 
             $( "#forecast_header" ).text( "5-Day Forecast:" );
+            $( "#card_row" ).text( "" );
             for ( var i = 0; i < 5; i++ ) {
                 d = new Date( d );
                 d.setDate( d.getDate() + 1 );
@@ -64,7 +70,7 @@ function queryCurrentWeather( cityName, apiKey, units, addListElement = false ) 
                 dailyCard.addClass( "daily_card col-md-2");
                 var cardDateHeader = $( "<h5>" ).text( `${ d.getMonth() + 1 }/${ d.getDate() }/${ d.getFullYear() }` );
                 var cardIcon = $( "<span>" ).text( "Icon Placeholder" );
-                var cardTemp = $( `<p>Temperature: ${ Math.round( ocResponse.daily[ i ].temp.day ) } °F<p>` );
+                var cardTemp = $( "<p>" ).text( `Temp: ${ Math.round( ocResponse.daily[ i ].temp.day ) } °F` );
                 var cardHumidity = $( `<p>Humidity: ${ ocResponse.daily[ i ].humidity }%</p>` );
 
                 dailyCard.append( cardDateHeader );
@@ -81,7 +87,7 @@ function queryCurrentWeather( cityName, apiKey, units, addListElement = false ) 
                 $( "#city_search" ).val( "" );
                 $( "#card_list" ).append( newCity );
                 $( ".sidebar_city" ).click( function() {
-                    queryCurrentWeather( $( this ).text(), APIkey, "imperial", null );
+                    queryCurrentWeather( $( this ).text(), APIkey, "imperial" );
                 });
             }
 
